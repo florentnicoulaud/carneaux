4ef4eda8a6d65c52736b5be3b11ca26d5dce2bfd
Florent Nicoulaud <fnicoulaud@norsys.fr>
2022-12-15 15:43:32 +0100

    refactor: no need for that many layers in this demo

--- api/src/api/routes/types.ts
+++ api/src/api/routes/types.ts
@@ -1,36 +1,35 @@
 import { Request, Response, Router } from 'express'
-import * as typeController from '../controllers/types'
+import * as typeDal from '../../db/dal/type'
 import Type from '../../db/models/Type'
 
 const typesRouter = Router()
 
 typesRouter.get(':/id', async (req: Request, res: Response) => {
   const id = Number(req.params.id)
-  const result = await typeController.getById(id)
+  const result = await typeDal.getById(id)
   return res.status(200).send(result)
 })
 typesRouter.put('/:id', async (req: Request, res: Response) => {
   const id = Number(req.params.id)
   const payload: Type = req.body
 
-  const result = await typeController.update(id, payload)
+  const result = await typeDal.update(id, payload)
   return res.status(201).send(result)
 })
 typesRouter.delete('/:id', async (req: Request, res: Response) => {
   const id = Number(req.params.id)
-
-  const result = await typeController.deleteById(id)
+  const result = await typeDal.deleteById(id)
   return res.status(204).send({
     success: result
   })
 })
 typesRouter.post('/', async (req: Request, res: Response) => {
   const payload: Type = req.body
-  const result = await typeController.create(payload)
+  const result = await typeDal.create(payload)
   return res.status(200).send(result)
 })
 typesRouter.get('/', async (req: Request, res: Response) => {
-  const results = await typeController.getAll()
+  const results = await typeDal.getAll()
   return res.status(200).send(results)
 })
 
