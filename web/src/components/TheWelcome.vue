ee03bc403a4fce47ffbcffaaa59ac049d65ff58c
Florent Nicoulaud <fnicoulaud@norsys.fr>
2022-12-19 09:54:37 +0100

    feat: component to fetch data from our API and display list of Types

---
+++ web/src/components/TheWelcome.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+import {onMounted, ref} from "vue";
+import {api} from "@/api/types-api";
+import Type from "@/models/Type";
+
+const types = ref<Type[]>([])
+
+onMounted(async () => {
+  types.value = await api.getTypes()
+})
+
+</script>
+
+<template>
+  ==> List des Types ( {{ types.length }}):
+
+  <div v-for="type in types" :key="`type-id-${type.id}`">
+    <h3 class="font-bold">
+      {{ type.nom }}
+    </h3>
+  </div>
+
+</template>
